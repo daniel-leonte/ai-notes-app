18004cd820198e3df2a8b69341d835e59aebb1ee
Daniel Leonte <leontedaniel37@gmail.com>
2025-03-02 19:40:35 +0200

    sentiment model load and predict

--- script.js
+++ script.js
@@ -40,3 +40,91 @@ function editNote(i) {
 
 // Load notes on start
 displayNotes();
+
+
+// === AI PART ===
+const url = {
+  modelUrl: 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json',
+  metadataUrl: 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/metadata.json'
+};
+
+const PAD_INDEX = 0;  // Index of the padding character.
+const OOV_INDEX = 2;  // Index fo the OOV character.
+
+let model, metadata;
+
+async function loadModel(url) {
+  try {
+    model = await tf.loadLayersModel(url.modelUrl);
+    console.log(model)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function loadMetadata(url) {
+  try {
+    const metadataJson = await fetch(url.metadataUrl);
+    metadata = await metadataJson.json();
+    console.log(metadata)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+loadModel(url);
+loadMetadata(url);
+
+// Pad and truncate all sequences to the same length
+const padSequences = (sequences, maxLen, padding = 'pre', truncating = 'pre', value = PAD_INDEX) => {
+  return sequences.map(seq => {
+    if (seq.length > maxLen) {
+      if (truncating === 'pre') {
+        seq.splice(0, seq.length - maxLen);
+      } else {
+        seq.splice(maxLen, seq.length - maxLen);
+      }
+    }
+
+    if (seq.length < maxLen) {
+      const pad = [];
+      for (let i = 0; i < maxLen - seq.length; ++i) {
+        pad.push(value);
+      }
+      if (padding === 'pre') {
+        seq = pad.concat(seq);
+      } else {
+        seq = seq.concat(pad);
+      }
+    }
+
+    return seq;
+  });
+}
+
+const getSentimentScore = (text = document.getElementById('note-input').value) => {
+  const inputText = text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' ');
+  console.log("inputText", inputText);
+
+  const sequence = inputText.map(word => {
+    console.log(metadata);
+    let wordIndex = metadata.word_index[word] + metadata.index_from;
+    if (wordIndex > metadata.vocabulary_size) {
+      wordIndex = OOV_INDEX;
+    }
+    return wordIndex;
+  });
+  console.log("sequence", sequence);
+
+  const paddedSequence = padSequences([sequence], metadata.max_len);
+
+  const input = tf.tensor2d(paddedSequence, [1, metadata.max_len]);
+  console.log("tensorInput", input);
+  
+  const predictOut = model.predict(input);
+  const score = predictOut.dataSync()[0];
+  predictOut.dispose();
+  
+  console.log('score', score);
+  return score;
+}
